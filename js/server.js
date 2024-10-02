648df3810f66ea8c0a4336bfa30a4e977d530478
Hilton Martins <hilton.martins@academico.ufpb.br>
2024-10-02 16:00:31 -0300

    fix server

--- js/server.js
+++ js/server.js
@@ -4,4 +4,4 @@ const serveStatic = require("serve-static");
 var app = connect();
 
 app.use(serveStatic('./', { index: ["index.html"] }));
-app.listen(3004, () => console.log("Server running on http://localhost:3004..."));
+app.listen(3000, () => console.log("Server running on http://localhost:3000..."));
